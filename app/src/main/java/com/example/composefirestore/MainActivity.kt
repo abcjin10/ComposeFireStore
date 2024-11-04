d41353f3058090f634ef4710b6bba58f34c44103
jin10 <st1034322@gmail.com>
2024-11-04 15:50:50 +0800

    v5_資料庫按鈕

--- app/src/main/java/com/example/composefirestore/MainActivity.kt
+++ app/src/main/java/com/example/composefirestore/MainActivity.kt
@@ -13,6 +13,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composefirestore.ui.theme.ComposeFireStoreTheme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +36,82 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFireStoreTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                   /* Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
+                    Birth(m = Modifier.padding(innerPadding))
+
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Birth(m: Modifier){
+    var userName by remember { mutableStateOf("陳錦璇")}
+    var userWeight by remember { mutableStateOf(3800)}
+    var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeFireStoreTheme {
-        Greeting("Android")
+    Column {
+        TextField(
+            value = userName,
+            onValueChange = { newText ->
+                userName = newText
+            },
+            modifier = m,
+                    label = { Text("姓名") },
+            placeholder = { Text("請輸入您的姓名") }
+
+        )
+
+
+
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+
+
+
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
+                + "\n密碼：$userPassword")
+        Row {
+            Button(onClick = {  }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {  }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {  }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
+
     }
+
+
 }
+
